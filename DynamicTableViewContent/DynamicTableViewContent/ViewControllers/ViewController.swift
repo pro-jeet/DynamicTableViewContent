baedea891e7cdb51138ddf20b4cb760f506105a7
Jitesh Sharma <professional.jitesh@gmail.com>
2019-01-14 11:04:15 +0530

    Initial Commit(Project Setup)
    1. Project Setup with File Structure.
    2. iOS Deployment target is 10.0.
    3. Development language is Swift.
    4. Using Core Data for DB.
    5. Unit Test and UI Test targets added.

---
+++ DynamicTableViewContent/DynamicTableViewContent/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicTableViewContent
+//
+//  Created by Jitesh Sharma on 14/01/19.
+//  Copyright © 2019 Jitesh Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
